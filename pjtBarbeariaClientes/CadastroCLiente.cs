1b7b8a018eb3a09f9bd3c2b9ba6e402cc39a2c00
GustavoFarias <162180731+GustavoFariasPereira@users.noreply.github.com>
2024-12-07 15:52:06 -0300

    Reapply "endereços ok"
    This reverts commit 1bef0c062b9afe0b1f1223e3141ba26a118502d8.

--- pjtBarbeariaClientes/CadastroCLiente.cs
+++ pjtBarbeariaClientes/CadastroCLiente.cs
@@ -14,6 +14,7 @@ namespace pjtBarbeariaClientes
     {
         List<Cliente> listaClientes = new List<Cliente>();
         Utilitarios utilitario = new Utilitarios();
+        String endereco;
 
         private void frmCadastroCLiente_Load(object sender, EventArgs e)
         {
@@ -71,7 +72,7 @@ namespace pjtBarbeariaClientes
                             cliente.email = txtEmail.Text;
 
                             listaClientes.Add(cliente);
-                            utilitario.salvarLista(listaClientes);
+                            utilitario.salvarLista(listaClientes, endereco);
                             MessageBox.Show("Cadastrado com sucesso!");
 
                             utilitario.abrirNovoForm<frmLogin>(this);
